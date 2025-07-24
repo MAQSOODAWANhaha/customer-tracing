1f118580b38371fc3d4a42021821712e8990388c
MAQSOOD AHMAD AWAN <04051613018@student.qau.edu.pk>
2025-07-24 05:54:19 +0000

    fix:customer group

--- backend/src/entities/customer_group.rs
+++ backend/src/entities/customer_group.rs
@@ -1,8 +1,8 @@
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use sea_orm::entity::prelude::*;
 
 /// 客户分组枚举
-#[derive(Debug, Clone, PartialEq, Eq, EnumIter, DeriveActiveEnum, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Eq, EnumIter, DeriveActiveEnum)]
 #[sea_orm(rs_type = "String", db_type = "String(StringLen::N(20))")]
 pub enum CustomerGroup {
     #[sea_orm(string_value = "团课")]
@@ -13,6 +13,41 @@ pub enum CustomerGroup {
     Personal,
     #[sea_orm(string_value = "教培")]
     Training,
+}
+
+impl Serialize for CustomerGroup {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        match self {
+            CustomerGroup::GroupClass => serializer.serialize_str("团课"),
+            CustomerGroup::SmallClass => serializer.serialize_str("小班"),
+            CustomerGroup::Personal => serializer.serialize_str("私教"),
+            CustomerGroup::Training => serializer.serialize_str("教培"),
+        }
+    }
+}
+
+impl<'de> Deserialize<'de> for CustomerGroup {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        match s.as_str() {
+            "团课" => Ok(CustomerGroup::GroupClass),
+            "小班" => Ok(CustomerGroup::SmallClass),
+            "私教" => Ok(CustomerGroup::Personal),
+            "教培" => Ok(CustomerGroup::Training),
+            // 为了兼容性，也支持英文变量名
+            "GroupClass" => Ok(CustomerGroup::GroupClass),
+            "SmallClass" => Ok(CustomerGroup::SmallClass),
+            "Personal" => Ok(CustomerGroup::Personal),
+            "Training" => Ok(CustomerGroup::Training),
+            _ => Err(serde::de::Error::unknown_variant(&s, &["团课", "小班", "私教", "教培"])),
+        }
+    }
 }
 
 impl Default for CustomerGroup {
